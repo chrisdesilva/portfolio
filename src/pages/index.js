6d9ecc5d4a2a573f4be6dc3ad09a7d8bf66b3cdb
unknown <chris.a.desilva@gmail.com>
2019-02-08 19:57:35 -0600

    Add placeholder cards to project page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,9 @@ import SEO from "../components/seo"
 const IndexPage = (props) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <div className="bg-purple sm:bg-green md:bg-blue">
-      <h1>Hey. I'm Chris DeSilva.</h1>  
+    <div className="bg-green">
+      <h1 className="text-center">Hey. I'm Chris DeSilva.</h1>
+      <p className="text-center my-5">I learned to code while teaching middle school.</p>  
     </div>
   </Layout>
 )
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,25 +1,72 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Img from 'gatsby-image'
 
-const ProjectsPage = () => (
+const ProjectsPage = (props) => (
   <Layout>
     <SEO title="Projects" />
     <h1 className="text-center">Projects</h1>
-    <div className="flex flex-col lg:flex-row my-5">
-      <div className="flex-1 text-grey-darker text-center bg-grey-light px-4 py-2 m-2">
-        thebanistersmusic.com
+    <div className="flex flex-col justify-around lg:flex-row m-5">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg mx-5 my-5">
+      <Img className="w-full" fluid={props.data.album.childImageSharp.fluid} alt="Sunset over water" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">thebanistersmusic.com</div>
+        <p className="text-grey-darker text-base">
+          Band website
+        </p>
       </div>
-      <div className="flex-1 text-grey-darker text-center bg-grey-light px-4 py-2 m-2">
-        mouph.com
+      <div className="px-6 py-4">
+        <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#bootstrap</span>
+        <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#css</span>
+        <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#javascript</span>
       </div>
-      <div className="flex-1 text-grey-darker text-center bg-grey-light px-4 py-2 m-2">
-        Practice Math
+    </div>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg m-5 ">
+      <Img className="w-full" fluid={props.data.album.childImageSharp.fluid} alt="Sunset over water" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">thebanistersmusic.com</div>
+        <p className="text-grey-darker text-base">
+          Band website
+        </p>
       </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#bootstrap</span>
+        <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#css</span>
+        <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#javascript</span>
+      </div>
+    </div>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg m-5">
+      <Img className="w-full" fluid={props.data.album.childImageSharp.fluid} alt="Sunset over water" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">thebanistersmusic.com</div>
+        <p className="text-grey-darker text-base">
+          Band website
+        </p>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#bootstrap</span>
+        <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#css</span>
+        <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#javascript</span>
+      </div>
+    </div>
     </div>
   </Layout>
 )
 
 export default ProjectsPage
+
+export const pageQuery = graphql`
+  query {
+    album: file(relativePath: { eq: "BlankDH.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
